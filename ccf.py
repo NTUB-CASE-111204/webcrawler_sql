d478d313abfcbf8027ca8cebb4b2bae2d9015f27
11036020 <11036020@ntub.edu.tw>
2022-07-25 22:19:45 +0800

    改寫leapingbunny與ccf的現存資料庫與官網資料比對(False)

--- ccf.py
+++ ccf.py
@@ -1,37 +1,32 @@
 # -!- coding: utf-8 -!-  。
 import psycopg2 
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-#import urllib.request as req
-import bs4
+from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
-#import time
-
 
 conn = psycopg2.connect(host="ec2-54-209-221-231.compute-1.amazonaws.com", user="ikojmqzefffjen", password ="079aad0bfbbc125c2f41389d7d65a83fe63f775aa42799b01120e8edb480ab2f", dbname="d28e9f04ls9tcu")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = conn.cursor()
+cursor2 = conn.cursor()
 print("資料庫連線成功！")
 
 #------------------------
 url = "https://crueltyfreeinternational.org/choose-cruelty-free-ccf-list-brands"
-#request=req.Request(url, headers={
-   # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36" #模仿真實使用者的資訊  
-#})
-#with req.urlopen(request) as response:
-    #data=response.read().decode("utf-8")
 r = requests.get("https://crueltyfreeinternational.org/choose-cruelty-free-ccf-list-brands")
-soup=bs4.BeautifulSoup(r.text, "html.parser")   #將網頁資料以html.parser
+soup = BeautifulSoup(r.text, "html.parser")   #將網頁資料以html.parser
 sel =soup.select("table a") #選取 table a ，並存入sel
 cursor.execute("SELECT b_name FROM public.brand")
 db = list(cursor.fetchall())
-
-uptime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print(db)
+cursor2.execute("SELECT b_name FROM public.brand WHERE ccf = True")
+ccfdb = list(cursor2.fetchall())
+print (len(ccfdb))
 list_a = []
 
 for name in sel:
         a_name = (name.text)
+        uptime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         if a_name =='':      #跳過空白的資料
             continue
         elif any(a_name in s for s in db):  #確認標題是否已存在資料庫
@@ -39,20 +34,17 @@ for name in sel:
                 a_name =a_name.replace("'","%")
                 cursor.execute("UPDATE public.brand SET ccf = %s, updatetime = '%s' where b_name like '%s'"%(True, uptime, a_name))    #更新資料ccf為True
                 #print("update///" + a_name)
-                #print(uptime)
             else:
                 cursor.execute("UPDATE public.brand SET ccf = %s, updatetime = '%s' where b_name like '%s'"%(True, uptime, a_name))   #更新資料ccf為True
                 #print("found/" + a_name)
-                #print(uptime)
         else:
-            print(a_name)
-            cursor.execute("INSERT INTO public.brand(b_name, ccf) VALUES (%s, %s);",(a_name, True))    #新增資料
+            cursor.execute("INSERT INTO public.brand(b_name, ccf, updatetime) VALUES (%s, %s, %s);",(a_name, True, uptime))    #新增資料
         list_a.append(a_name)     #將a_name放進list最後
         #print(list_a)
         
 j=0   
-while (j < len(db)):
-    te = str(db[j]).strip('(,)')  #去除資料庫前後的(,)符號
+while (j < len(ccfdb)):
+    te = str(ccfdb[j]).strip('(,)')  #去除資料庫前後的(,)符號
     te = te.replace("'", "")    #將'改為空白
     te = te.replace('"', '')    #將"改為空白
     if(te not in list_a):       #若資料庫的資料不存在於list_a
--- leapingbunny.py
+++ leapingbunny.py
@@ -4,12 +4,13 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 #import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from datetime import datetime
 import time
+from datetime import datetime
 
 conn = psycopg2.connect(host="ec2-54-209-221-231.compute-1.amazonaws.com", user="ikojmqzefffjen", password ="079aad0bfbbc125c2f41389d7d65a83fe63f775aa42799b01120e8edb480ab2f", dbname="d28e9f04ls9tcu")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = conn.cursor()
+cursor2 = conn.cursor()
 print("資料庫連線成功！")
 
 #------------------------
@@ -21,13 +22,13 @@ default_url = "https://www.leapingbunny.org/shopping-guide"
 driver.get(default_url)
     
 cursor.execute("SELECT b_name FROM public.brand")
-
 db = list(cursor.fetchall())
-
-uptime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print(db)
-
+cursor2.execute("SELECT b_name FROM public.brand WHERE leapingbunny = True")
+leapingbunnydb = list(cursor2.fetchall())
+print (len(leapingbunnydb))
 list_a = []
+
     
 for i in range(1,2):
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
@@ -38,52 +39,31 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 for block in soup.select('.field-content a'):
     #print(block.text)
     for name in block:
-        cursor = conn.cursor()
         a_name = (name.text)
+        uptime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         if a_name =='':
             continue
         elif any(a_name in s for s in db):
             if(a_name.count("'") >= 1):
                 a_name = a_name.replace("'", "%")
                 cursor.execute("UPDATE public.brand SET leapingbunny = %s, updatetime = '%s' where b_name like '%s'"%(True, uptime, a_name)) 
-                
                 #print(a_name)
-                
             else:
                 cursor.execute("UPDATE public.brand SET leapingbunny = %s, updatetime = '%s' where b_name like '%s'"%(True, uptime, a_name)) 
-                
                 #print("found/" + a_name)
         else:
             print(a_name)
-            cursor.execute("INSERT INTO public.brand(b_name, leapingbunny) VALUES (%s, %s);",(a_name, True))
-
-            
+            cursor.execute("INSERT INTO public.brand(b_name, leapingbunny, updatetime) VALUES (%s, %s, %s);",(a_name, True, uptime))  
         list_a.append(a_name)     
 j=0   
-while (j < len(db)):
-    te = str(db[j]).strip('(,)')  #去除資料庫前後的(,)符號
+while (j < len(leapingbunnydb)):
+    te = str(leapingbunnydb[j]).strip('(,)')  #去除資料庫前後的(,)符號
     te = te.replace("'", "")    #將'改為空白
     te = te.replace('"', '')    #將"改為空白
     if(te not in list_a):       #若資料庫的資料不存在於list_a
         print(te)
-        cursor.execute("UPDATE public.brand SET leapingbunny = False where b_name like '%s'"%(te))      #更新資料peta為False
+        cursor.execute("UPDATE public.brand SET leapingbunny = False where b_name like '%s'"%(te)) #更新資料leapingbunny為False
     j+=1   
-print(list_a) 
+#print(list_a) 
 driver.quit()
- 
-
-
-#print(df.strftime('%Y-%m-%d %H:%M:%S'))
-#sql_insert=sql_insert="INSERT into public.brand(updatetime) values(str_to_date(\'%s\','%%Y-%%m-%%d %%H:%%i:%%s'))" %(df.strftime("%Y-%m-%d %H:%M:%S"))
-#cursor.execute("INSERT INTO public.brand(updatetime) VALUES (%s)",(now))
-#cursor.execute("UPDATE public.brand SET leapingbunny SET updatetime = '%s'"%(now))
-
-# 取得現在時間
-
-#txt = '上次更新時間為：' + str(now)
-
-#轉成df
-#df = pandas.DataFrame([txt], index=['UpdateTime'])
-
-# 存出檔案
-#df.to_csv('log.csv', header=False)
+print('資料新增成功！')
